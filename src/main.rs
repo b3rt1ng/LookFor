773def49e3b7412a658bf781de5cc29e11999a70
Antoine <43414019+b3rt1ng@users.noreply.github.com>
2024-12-17 23:02:13 +0100

    Update main.rs
    counter

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use writer::MultiWriter;
 use std::io;
 use std::io::Write;
 use clap::Parser;
+use std::collections::HashMap;
+use colored::*;
 
 fn main() {
     let args = Args::parse();
@@ -23,6 +25,12 @@ fn main() {
 
     let keywords: Vec<String> = args.find.split(',').map(String::from).collect();
 
+    // Compteur pour suivre les occurrences
+    let mut keyword_counts: HashMap<String, usize> = keywords
+        .iter()
+        .map(|keyword| (keyword.clone(), 0))
+        .collect();
+
     if args.path.is_dir() {
         writeln!(
             &mut multi_writer,
@@ -30,7 +38,14 @@ fn main() {
             args.path.display()
         )
         .unwrap();
-        search_in_directory(&keywords, &args.path, args.noshow, args.maxsize, &mut multi_writer);
+        search_in_directory(
+            &keywords,
+            &args.path,
+            args.noshow,
+            args.maxsize,
+            &mut multi_writer,
+            &mut keyword_counts,
+        );
     } else if args.path.is_file() {
         writeln!(
             &mut multi_writer,
@@ -38,10 +53,31 @@ fn main() {
             args.path.display()
         )
         .unwrap();
-        if let Err(e) = search_in_file(&keywords, &args.path, args.noshow, args.maxsize, &mut multi_writer) {
+        if let Err(e) = search_in_file(
+            &keywords,
+            &args.path,
+            args.noshow,
+            args.maxsize,
+            &mut multi_writer,
+            &mut keyword_counts,
+        ) {
             if !args.noshow {
                 eprintln!("Error with the file {}: {}", args.path.display(), e);
             }
         }
+    }
+
+    // Affichage des résultats du compteur
+    writeln!(&mut multi_writer, "\nSummary:").unwrap();
+    for (keyword, count) in &keyword_counts {
+        let times_str = if *count > 1 { "times" } else { "time" };
+        writeln!(
+            &mut multi_writer,
+            "found {}: {} {}",
+            keyword.red().bold(),
+            count.to_string().green().bold(),
+            times_str
+        )
+        .unwrap();
     }
 }
